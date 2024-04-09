2b4297b50cbcbda8f210596df63faf755e3f04eb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-10 00:52:26 +0530

    A1Q2 added , sum of two numbers  added.

---
+++ Assignments/src/A1Q2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A1Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = sc.nextInt();
+        System.out.println("The sum is : "+ (num1+num2));
+        sc.close();
+    }
+}
