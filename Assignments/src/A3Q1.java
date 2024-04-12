f7824018896f485502136d36fc4ec0ea789ac4bc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-12 22:01:12 +0530

    A3Q1 added , Area Of Circle Java Program

---
+++ Assignments/src/A3Q1.java
@@ -0,0 +1,12 @@
+// Area Of Circle Java Program
+import java.util.*;
+public class A3Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius of the circle : ");
+        double r = sc.nextDouble();
+        System.out.println("Area of the circle is: "+ (Math.PI*r*r));
+        sc.close();
+
+    }
+}
