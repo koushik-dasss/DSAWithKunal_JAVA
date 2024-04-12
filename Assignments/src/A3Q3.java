c6b2a02ea6bd2cff734dfbac3d95582cbec9a91f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-12 22:08:51 +0530

    A3Q3 added , Area Of Rectangle Program.

---
+++ Assignments/src/A3Q3.java
@@ -0,0 +1,14 @@
+// Area Of Rectangle Program
+import java.util.*;
+public class A3Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length : ");
+        double l = sc.nextDouble();
+        System.out.println("Enter breadth: ");
+        double b = sc.nextDouble();
+        System.out.println("Area of rectangle is: "+ (l*b));
+        sc.close();
+
+    }
+}
