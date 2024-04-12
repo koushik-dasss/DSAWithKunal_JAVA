5453e30e15dea87d3fa30369bfb069c95add9439
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-12 22:04:50 +0530

    A3Q2 added , Area Of Triangle.

---
+++ Assignments/src/A3Q2.java
@@ -0,0 +1,14 @@
+// Area Of Triangle
+import java.util.*;
+public class A3Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base : ");
+        double b = sc.nextDouble();
+        System.out.println("Enter height : ");
+        double h = sc.nextDouble();
+        System.out.println("Area of triangle is : "+ (0.5*b*h));
+
+        sc.close();
+    }
+}
