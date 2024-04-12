3bac724b24ca1eecd7872648ab6ad9e839443f90
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-13 00:25:40 +0530

    A3Q5 added , Area Of ||gm Program using base and height.

---
+++ Assignments/src/A3Q5.java
@@ -0,0 +1,13 @@
+// Area Of Parallelogram
+import java.util.*;
+public class A3Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base : ");
+        double b = sc.nextDouble();
+        System.out.println("Enter height : ");
+        double h = sc.nextDouble();
+        System.out.println("Area of ||gm is : "+ (0.5*b*h));
+        sc.close();
+    }
+}
