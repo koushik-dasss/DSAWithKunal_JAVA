62951d5e105eac67e5970125f41b84291c0f3f2a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 18:31:58 +0530

    A3Q26 added , Sum of 2 numbers    .

---
+++ Assignments/src/A3Q26.java
@@ -0,0 +1,12 @@
+// Addition Of Two Numbers
+import java.util.*;
+public class A3Q26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum is : "+(a+b));
+        sc.close();
+    }
+}
