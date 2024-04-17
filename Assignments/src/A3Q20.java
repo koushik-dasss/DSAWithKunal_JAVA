df239178ec424c50e2115dffb227faa310465603
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 18:11:42 +0530

    A3Q20 added , TSA of cube   .

---
+++ Assignments/src/A3Q20.java
@@ -0,0 +1,11 @@
+// Total Surface Area Of Cube
+import java.util.*;
+public class A3Q20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the cube : ");
+        double a = sc.nextDouble();
+        System.out.println("TSA of the cube is : "+(6*a*a));
+        sc.close();
+    }
+}
