503d85caba6d6b021bf79945b992f2bb43d3f92a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 08:55:19 +0530

    A3Q9 added , Perimeter Of Equilateral triangle.

---
+++ Assignments/src/A3Q9.java
@@ -0,0 +1,11 @@
+// Perimeter Of Equilateral Triangle
+import java.util.*;
+public class A3Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the equilateral  triangle : ");
+        int n = sc.nextInt();
+        System.out.println("The perimeter of the equilateral triangle is : "+(3*n));
+        sc.close();
+    }
+}
