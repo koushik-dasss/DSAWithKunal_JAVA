b7529f3d9a74d59152153d840142e23820e2af95
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 09:11:36 +0530

    A3Q13 added , Perimeter Of Rhombus  .

---
+++ Assignments/src/A3Q13.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A3Q13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the rhombus : ");
+        int a = sc.nextInt();
+        System.out.println("The perimeter of the rhombus is : "+ (4*a));
+        sc.close();
+
+    }
+}
