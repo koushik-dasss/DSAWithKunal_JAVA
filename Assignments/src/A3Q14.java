e3a45125a48c1733469e0780f44ec4266c2ee999
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 17:17:16 +0530

    A3Q7 added , Volume of Cone .

---
+++ Assignments/src/A3Q14.java
@@ -0,0 +1,13 @@
+// Volume Of Cone Java Program
+import java.util.*;
+public class A3Q14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the cone : ");
+        double r = sc.nextDouble();
+        System.out.println("Enter the height of the cone : ");
+        double h = sc.nextDouble();
+        System.out.println("The volume of the cone is : "+ (1/3.0)*Math.PI*r*r*h);
+        sc.close();
+    }
+}
