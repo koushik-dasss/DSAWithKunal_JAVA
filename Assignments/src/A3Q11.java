d871d16753127c672fc3c3c5c2d548a1b76fca3e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 09:05:08 +0530

    A3Q11 added , Perimeter Of Rectangle .

---
+++ Assignments/src/A3Q11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A3Q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle : ");
+        int l = sc.nextInt();
+        System.out.println("Enter the breadth of the rectangle : ");
+        int b = sc.nextInt();
+        System.out.println("The perimeter of the rectangle is : "+2*(l+b));
+        sc.close();
+    }
+}
