1c16f7f5078b35dc636e09e99f86d872f3604ca3
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 09:08:23 +0530

    A3Q12 added , Perimeter Of Square  .

---
+++ Assignments/src/A3Q12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A3Q12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the square : ");
+        int s = sc.nextInt();
+        System.out.println("The perimeter of the square is : "+ (4*s));
+        sc.close();
+
+    }
+}
