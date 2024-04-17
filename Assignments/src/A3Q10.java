0194356b689840b5e2b0ca61c7d30674df9a6c6e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-04-17 09:01:42 +0530

    A3Q10 added , Perimeter Of ||gm.

---
+++ Assignments/src/A3Q10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A3Q10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the ||gm : ");
+        int s = sc.nextInt();
+        System.out.println("Enter the base of the ||gm : ");
+        int b = sc.nextInt();
+        System.out.println("The perimeter of the ||gm ids : "+ 2*(b+s));
+        sc.close();
+    }
+}
